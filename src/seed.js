2d55cd9b1ed9f903643e56c2701b58dc4e347ce9
VS800624 <vishalsingh800624@gmail.com>
2026-01-22 17:41:18 +0530

    Add seed data in database

---
+++ src/seed.js
@@ -0,0 +1,24 @@
+require("dotenv").config()
+const mongoose = require("mongoose")
+const connectDB = require("./config/database")
+const PopularDestination = require("./models/popularDestination")
+const data = require("./utils/popularCountries.json")
+
+const seedData = async() => {
+  try{
+    await connectDB()
+    console.log("Database Connected")
+
+    await PopularDestination.deleteMany()
+    await PopularDestination.insertMany(data)
+    console.log("Data Added Successfully")
+  } catch(error){
+    console.log("Seeding failed: ", error)
+    process.exit(1)
+  } finally{
+    await mongoose.connection.close()
+    process.exit(0)
+  }
+}
+
+seedData()
