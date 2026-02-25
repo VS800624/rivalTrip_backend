79764170b98420e0417095faec89f9ecdb707dd3
VS800624 <vishalsingh800624@gmail.com>
2026-02-25 11:34:17 +0530

    Add get users by id route

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const adminAuth = require("../middleware/auth")
 const User = require("../models/user")
+const { default: mongoose } = require("mongoose")
 const userRouter = express.Router()
 
 // get all users
@@ -20,6 +21,31 @@ userRouter.get("/admin/users",
       
     }catch(err){
       res.status(500).json({status: false , message:err.message})
+    }
+  }
+)
+
+// get user by id
+userRouter.get("/admin/user/:id",
+  // adminAuth,
+  async(req,res) => {
+    try{
+      // Check if ID is valid
+      if(!mongoose.Types.ObjectId.isValid(req.params.id)){
+        return res.status(400).json({message:"Invalid Id"})
+      }
+
+      // Find user
+      const user = await User.findById(req.params.id)
+
+      if(!user){
+        return res.status(404).json({message: "User not found"})
+      }
+
+      res.json({message: "Fetched user successfully", user})
+      
+    } catch(err){
+      res.status(500).json({status:false, message: err.message})
     }
   }
 )
