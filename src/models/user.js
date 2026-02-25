1bbb10abad8dfe6aa9215083c8d6de4432de9627
VS800624 <vishalsingh800624@gmail.com>
2026-02-25 10:46:12 +0530

    Improve user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,68 +1,75 @@
-const mongoose = require("mongoose")
-const validator = require("validator")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const mongoose = require("mongoose");
+const validator = require("validator");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    maxlength: 25,
-    minlength: 3,
-    trim: true
-  },
-  lastName: {
-    type: String,
-    required: true,
-    maxlength: 25,
-    minlength: 3,
-    trim : true
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true,
-    lowercase: true,
-    validate(value){
-      if(!validator.isEmail(value)){
-        throw new Error("Invalid Email Address: "+ value)
-      }
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      maxlength: 25,
+      minlength: 3,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      maxlength: 25,
+      minlength: 3,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Invalid Email Address: " + value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false, //select: false hides password in queries i.e Do NOT return password field in normal queries
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Please enter a strong password");
+        }
+      },
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    status: {
+      type: String,
+      enum: ["active", "blocked", "inactive"],
+      default: "active",
     },
   },
-  password: {
-    type: String,
-    required: true,
-    select: false,   //select: false hides password in queries i.e Do NOT return password field in normal queries
-    validate(value){
-      if(!validator.isStrongPassword(value)){
-        throw new Error("Please enter a strong password");
-      }
-    }
-  },
-  role: {
-    type: String,
-    enum: ["user", "admin"],
-    default: "user"
-  }
-}, {timestamps: true})
+  { timestamps: true },
+);
 
 // Create login token:
 // Creates a JWT token for the user
 // Token contains: User _id
 // Token expires in 1 day
 // This token proves the user is logged in
-userSchema.methods.generateAuthToken = async function(){
+userSchema.methods.generateAuthToken = async function () {
   const user = this;
   // jwt.sign(payload, secret, options)
-  const token = await jwt.sign({_id: user._id}, process.env.JWT_SECRET, {
-    expiresIn: "1d"
-  })
-  return token
-}
+  const token = await jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+  return token;
+};
 
-userSchema.methods.validatePassword = async function(passwordInputByUser){
+userSchema.methods.validatePassword = async function (passwordInputByUser) {
   if (!passwordInputByUser) {
     throw new Error("Password not provided");
   }
@@ -71,12 +78,14 @@ userSchema.methods.validatePassword = async function(passwordInputByUser){
     throw new Error("User password not found");
   }
   const user = this;
-  const passwordHash = user.password
+  const passwordHash = user.password;
   // bcrypt.compare(plainText, hash)
-  const isPasswordValid = await bcrypt.compare(passwordInputByUser, passwordHash)
-  return isPasswordValid
-}
+  const isPasswordValid = await bcrypt.compare(
+    passwordInputByUser,
+    passwordHash,
+  );
+  return isPasswordValid;
+};
 
-
-const User = mongoose.model("User", userSchema)
+const User = mongoose.model("User", userSchema);
 module.exports = User;
